2c0a42d859a402aaa1026051458eb9d260d10265
Dmitry Sadovnikov <sadovnikov.js@gmail.com>
2018-01-07 15:40:58 +0300

    update

--- Blockchain/solidity.sol
+++ Blockchain/solidity.sol
@@ -32,3 +32,92 @@ contract HelloWorldContract {
         return issuer;
     }
 }
+
+////////////////////
+
+contract PayableContract {
+    function PayableContract() {
+
+    }
+
+    function receiveFunds() payable {
+
+    }
+
+    function getter() constant returns(uint) {
+        return this.balance;
+    }
+}
+
+////////////////////
+
+contract CustodialContract {
+    address client;
+    bool public _switch = false;
+
+    event UpdateStatus(string _msg, uint _amount);
+    event UserStatus(string _msg, address user, uint amount);
+
+    function CustodialContract() {
+        client = msg.sender;
+    }
+
+    modifier ifClient() {
+        if(msg.sender != client) {
+            throw;
+        }
+        _;
+    }
+
+    function depositFunds() payable {
+        UserStatus('User transferred some money', msg.sender, msg.value);
+
+    }
+
+    function withdrawFunds(uint amount) ifClient {
+        if(client.send(amount)) {
+            UpdateStatus('User transferred some money', 0);
+            _switch = true;
+        } else {
+            _switch = false;
+        }
+    }
+
+    function getFunds() ifClient constant returns(uint) {
+        return this.balance;
+    }
+}
+
+////////////////////
+
+contract CallerContract {
+//    CallerContract toBeCalled = new CalledContract();
+    CallerContract toBeCalled = CalledContract(0x0899887678);
+
+    function getNumber() constant returns(uint) {
+        return toBeCalled.getNumber();
+    }
+
+    function getWords() constant returns(bytes32) {
+        return toBeCalled.getWords();
+    }
+}
+
+contract CalledContract {
+    uint number = 42;
+    bytes32 words = "Hello World";
+
+    function getNumber() constant returns(uint) {
+        return number;
+    }
+
+    function setNumber(uint _number) {
+        number = _number;
+    }
+
+
+    function getWords() constant returns(bytes32) {
+        return words;
+    }
+}
+
--- JavaScript/node.js
+++ JavaScript/node.js
@@ -7,4 +7,51 @@ fs.appendFile('hello.txt', '\nSome more content', function(err) {
 
 // Promise – это специальный объект, который содержит своё состояние. Вначале pending («ожидание»), 
 // затем – одно из: fulfilled («выполнено успешно») или rejected («выполнено с ошибкой»).
-// ПРОМИСЫ нужны для ассинхронной работы  
+// ПРОМИСЫ нужны для ассинхронной работы
+
+
+// MVC
+// app.js
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res)) => {
+  res.send('Hello');
+  res.sendFile(__dirname + '/public/index.html');
+})
+
+app.listen(3000);
+
+// package.json
+{
+    "name": 'lol',
+    "version": '1.0.0',
+    "main": "app.js",
+    "license": "MIT",
+    "scripts": {
+      "start": "node app.js"
+  },
+    "dependencies": {
+      "express": "^.4.16.2"
+  }
+}
+
+// index.html
+<button onclick="contactServer()">Click here</button>
+<script>
+  function contactServer() {
+    var xttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("response").innerHTML = this.responseText;
+      }
+    };
+    xhttp.open("GET", "ajax_info.txt", true);
+    xhttp.send();
+  }
+</script>
+
+// npm install -g ethereumjs-testrpc
+// testrpc
+// yarn start
+
