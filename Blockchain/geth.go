0cfbc90baa769b171193f62e1b8742b1f235c885
Dmitry Sadovnikov <sadovnikov.js@gmail.com>
2018-01-28 20:26:39 +0300

    geth

--- Blockchain/geth.go
+++ Blockchain/geth.go
@@ -1,2 +1,25 @@
 geth --testnet --fast
 cd Library/Ethereum/testnet/
+
+geth —help
+
+geth --networkid 3
+geth --testnet (ROPSTEN)
+
+
+geth --networkid 4
+geth —rinkeby (RINKEBY)
+
+
+geth —rinkeby —datadir “./data/rinkeby” —syncmode “light” —verbosity 4
+
+
+// show config
+geth dumpconfig
+
+// backup default config
+geth dumpconfig > /Users/d.sadovnikov/Desktop/repo/Gists/Blockchain/default.toml
+
+// use different config
+geth —rinkeby —datadir “./data/rinkeby” —syncmode “light” —verbosity 4 > test.toml
+geth --config test.toml
